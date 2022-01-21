93d1211fc3c5748e0bd87b3264cc54e784999336
antieatingactivist <antieatingactivist@gmail.com>
2022-01-20 22:45:34 -0800

    added license menu

--- index.js
+++ index.js
@@ -23,7 +23,13 @@ inquirer
             type: 'input',
             message: 'Enter test instructions...',
             name: 'testInstructions'
+        },{
+            type: 'list',
+            message: 'Which license would you like to use?',
+            name: 'license',
+            choices: ['GNU GPL v3.0', 'MIT', 'BSD 2-Clause', 'BSD 3-Clause', 'Boost Software license','Creative Commons Zero 1.0','Eclipse Public License','Mozilla Public License','The Unilicense'],
         },
+      
 
 
 
